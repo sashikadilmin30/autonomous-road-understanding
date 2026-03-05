b25760da4d4c797cbab1412502f77f4462babaeb
sashikadilmin30 <shashikadilmin30@gmail.com>
2026-03-05 12:57:25 +0530

    Stage 2: Region of Interest masking for road detection

--- src/lane_detection.py
+++ src/lane_detection.py
@@ -1,7 +1,49 @@
-
 import cv2
 import numpy as np
 
-print("OpenCV working:", cv2.__version__)
+# Load image
+image = cv2.imread("data/road.jpg")
 
+if image is None:
+    print("Error: Image not found")
+    exit()
 
+# Resize image
+image = cv2.resize(image, (800, 500))
+
+# Convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Blur
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+# Edge detection
+edges = cv2.Canny(blur, 50, 150)
+
+# -------- REGION OF INTEREST --------
+
+height = edges.shape[0]
+width = edges.shape[1]
+
+mask = np.zeros_like(edges)
+
+polygon = np.array([[
+    (0, height),
+    (width, height),
+    (width//2, height//2)
+]])
+
+cv2.fillPoly(mask, polygon, 255)
+
+roi = cv2.bitwise_and(edges, mask)
+
+# ------------------------------------
+
+cv2.imshow("Original", image)
+cv2.imshow("Edges", edges)
+cv2.imshow("ROI", roi)
+
+cv2.imwrite("../results/roi_edges.jpg", roi)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
